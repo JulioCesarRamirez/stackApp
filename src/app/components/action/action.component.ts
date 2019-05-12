e9c535e3ecbe9a295e886aa07a3168bdf6290f94
Juliocesarramirez <julio_marshal@hotmail.com>
2019-05-12 10:35:38 -0500

    feat(action,home): added logic to buy or sell

--- src/app/components/action/action.component.ts
+++ src/app/components/action/action.component.ts
@@ -8,15 +8,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ActionComponent implements OnInit {
 
   @Input() public type: string;
+  @Input() public price: number;
   public typeName: string;
   public hold: boolean = false;
   public buy: boolean = false;
   public sell: boolean = false;
-
   constructor() { }
 
   ngOnInit() {
     this.typeConverter();
+  }
+
+  getNumber(value: number) {
+    this.price = this.price * value;    
   }
 
   typeConverter() {
